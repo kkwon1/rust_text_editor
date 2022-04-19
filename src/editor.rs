15098e6235ebd4b51435a3f5e79eba9becbe2894
Kevin Kwon <kevinkwon1995@gmail.com>
2022-04-18 20:22:06 -0700

    Creating the interface for text edit

--- src/editor.rs
+++ src/editor.rs
@@ -1,25 +1,47 @@
+use crossterm::event;
 use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{event, terminal};
+use crossterm::{cursor, terminal, ExecutableCommand};
+use std::io::Write;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         // Enable raw mode so that characters are directly read from and written to the device
         terminal::enable_raw_mode().expect("Failed to enable Raw mode");
 
         loop {
-            if let Err(_) = self.process_keypress() {
+            if let Err(_) = self.refresh_screen() {
+                crash_and_burn();
+            }
+
+            if self.should_quit {
                 break;
+            } else {
+                self.draw_rows();
+                std::io::stdout().execute(cursor::MoveTo(0, 0)).ok();
+            }
+
+            if let Err(_) = self.process_keypress() {
+                crash_and_burn();
             }
         }
     }
 
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        std::io::stdout()
+            .execute(terminal::Clear(terminal::ClearType::All))?
+            .execute(cursor::MoveTo(0, 0))?
+            .flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         if let Event::Key(pressed_key) = read_key()? {
             match pressed_key {
                 KeyEvent {
@@ -31,7 +53,8 @@ impl Editor {
                     modifiers: event::KeyModifiers::NONE,
                 } => {
                     exit_gracefully();
-                    Err(std::io::Error::new(std::io::ErrorKind::Interrupted, "Exit"))
+                    self.should_quit = true;
+                    Ok(())
                 }
                 _ => {
                     println!("{:?} \r", pressed_key);
@@ -40,6 +63,12 @@ impl Editor {
             }
         } else {
             Ok(())
+        }
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
         }
     }
 }
@@ -51,4 +80,9 @@ fn read_key() -> Result<Event, std::io::Error> {
 fn exit_gracefully() {
     crossterm::terminal::disable_raw_mode().expect("Failed to disable Raw mode");
     println!("Goodbye!");
+}
+
+fn crash_and_burn() {
+    println!("{}", terminal::Clear(terminal::ClearType::All));
+    panic!("Program crashed!");
 }
