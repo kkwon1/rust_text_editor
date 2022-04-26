422175222cccab98f799b75b68a6925338c9f252
Kevin Kwon <kevinkwon1995@gmail.com>
2022-04-25 22:14:48 -0700

    Ability to read files

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,10 @@
+use crate::Document;
+use crate::Row;
 use crate::Terminal;
-use crossterm::event;
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use std::env;
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -11,6 +14,8 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    offset: Position,
+    document: Document,
 }
 
 impl Editor {
@@ -31,22 +36,35 @@ impl Editor {
     }
 
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            document: document,
+            cursor_position: Position::default(),
+            offset: Position::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default());
 
         if self.should_quit {
             Terminal::quit();
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            })
         }
 
         Terminal::cursor_show();
@@ -59,7 +77,7 @@ impl Editor {
                 (KeyModifiers::CONTROL, KeyCode::Char('q')) | (_, KeyCode::Esc) => {
                     Terminal::quit();
                     self.should_quit = true;
-                    Ok(())
+                    return Ok(());
                 }
                 (_, KeyCode::Up)
                 | (_, KeyCode::Down)
@@ -70,20 +88,42 @@ impl Editor {
                 | (_, KeyCode::End)
                 | (_, KeyCode::Home) => {
                     self.move_cursor(pressed_key);
-                    Ok(())
                 }
-                _ => Ok(()),
+                _ => (),
             }
-        } else {
-            Ok(())
+        }
+        self.scroll();
+        Ok(())
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
         }
     }
 
     fn move_cursor(&mut self, key_event: KeyEvent) {
+        let terminal_height = self.terminal.size().height as usize;
         let Position { mut x, mut y } = self.cursor_position;
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
 
         match (key_event.modifiers, key_event.code) {
             (_, KeyCode::Up) => y = y.saturating_sub(1),
@@ -92,28 +132,73 @@ impl Editor {
                     y = y.saturating_add(1);
                 }
             }
-            (_, KeyCode::Left) => x = x.saturating_sub(1),
-            (_, KeyCode::Right) => {
-                if x < width {
-                    x = x.saturating_add(1);
+            (_, KeyCode::Left) => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    if let Some(row) = self.document.row(y) {
+                        x = row.len();
+                    } else {
+                        x = 0;
+                    }
                 }
             }
-            (_, KeyCode::PageUp) => y = 0,
-            (_, KeyCode::PageDown) => y = height,
+            (_, KeyCode::Right) => {
+                if x < width {
+                    x += 1;
+                } else if y < height {
+                    y += 1;
+                    x = 0;
+                }
+            }
+            (_, KeyCode::PageUp) => {
+                y = if y > terminal_height {
+                    y - terminal_height
+                } else {
+                    0
+                }
+            }
+            (_, KeyCode::PageDown) => {
+                y = if y.saturating_add(terminal_height) < height {
+                    y + terminal_height as usize
+                } else {
+                    height
+                }
+            }
             (_, KeyCode::Home) => x = 0,
             (_, KeyCode::End) => x = width,
             _ => (),
         }
 
+        width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+        if x > width {
+            x = width;
+        }
+
         self.cursor_position = Position { x, y };
+    }
+
+    fn draw_row(&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
+        let row = row.render(start, end);
+        println!("{}\r", row)
     }
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
-                self.draw_welcome_message();
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
+                self.draw_welcome_message()
             } else {
                 println!("~\r");
             }
---
+++ src/row.rs
@@ -0,0 +1,39 @@
+use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
+
+pub struct Row {
+    string: String,
+}
+
+impl From<&str> for Row {
+    fn from(slice: &str) -> Self {
+        Self {
+            string: String::from(slice),
+        }
+    }
+}
+
+impl Row {
+    pub fn render(&self, start: usize, end: usize) -> String {
+        let end = cmp::min(end, self.string.len());
+        let start = cmp::min(start, end);
+
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            result.push_str(grapheme);
+        }
+        result
+    }
+
+    pub fn len(&self) -> usize {
+        self.string[..].graphemes(true).count()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.string.is_empty()
+    }
+}
